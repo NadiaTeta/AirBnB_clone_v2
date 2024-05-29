439fd8d1e003c8c691a300592cb8a09ff7b6ed68
NadiaTeta <144005791+NadiaTeta@users.noreply.github.com>
2024-05-29 14:40:09 +0200

    s

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Defines unittests for console.py."""
+import sys
 import os
+sys.path.insert(0, os.path.abspath('..'))
+
 import unittest
 from unittest.mock import patch
 from io import StringIO
