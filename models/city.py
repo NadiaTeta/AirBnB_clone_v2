2099aef6160c68f8bc63c4036e2a2f5dc2319633
NadiaTeta <144005791+NadiaTeta@users.noreply.github.com>
2024-06-02 16:06:22 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-from models.place import Place
+""" City Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """This city class, contains state ID and name"""
-    state_id= ""
-    name= ""
+class City(BaseModel):
+    """ The city class, contains state ID and name """
+    state_id = ""
+    name = ""
