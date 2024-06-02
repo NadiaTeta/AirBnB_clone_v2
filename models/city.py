87f922e4c388cfd30068414391578ddba4455cab
NadiaTeta <144005791+NadiaTeta@users.noreply.github.com>
2024-06-02 14:28:58 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -9,13 +9,6 @@ from models.place import Place
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    """This city class, contains state ID and name"""
+    state_id= ""
+    name= ""
