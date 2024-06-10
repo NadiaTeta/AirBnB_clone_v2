626cffa134328c2f2cf19aa4ed4b48ec7340f520
NadiaTeta <144005791+NadiaTeta@users.noreply.github.com>
2024-06-10 13:05:04 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,6 +26,6 @@ echo "<!DOCTYPE html>
 
 sudo rm -rf /data/web_static/current
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R tetabianca:tetabianca /data/
 sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
